64abb5ba12a23b0e64c165f2a2e313c6155333eb
Meryellen <meryellen.adm@outlook.com>
2024-01-30 16:28:33 -0300

    Atividade 1
    Criar um cadastro de pessoas com os dados abaixo:
    1) nome, salário, idade e se possui diploma

---
+++ Atividade1.js
@@ -0,0 +1,16 @@
+/* Criar um cadastro de pessoas com os dados abaixo: 
+1) nome, salário, idade e se possui diploma
+
+Devemos identificar os tipos de cada variável. */
+
+// Definindo variáveis e seus tipos
+let nome = "Lucas"; // String para o nome
+let salario = 100; // Number para o salário (pode ser um número de ponto flutuante)
+let idade = 27; // Number para a idade
+let possuiDiploma = true; // Boolean para indicar se possui diploma
+
+// Exibindo os dados no console
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui Diploma? " + possuiDiploma);
